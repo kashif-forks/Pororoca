8d657aa06d2316fcbcc6bb289c4f7d2f13684856
Alexandre H.T.R. Bonfitto <alexandrehtrb@outlook.com>
2025-10-16 17:20:28 -0300

    feature: show 'loading...' message on top-right corner, on app startup

--- src/Pororoca.Desktop/ViewModels/MainWindowViewModel.cs
+++ src/Pororoca.Desktop/ViewModels/MainWindowViewModel.cs
@@ -29,7 +29,10 @@ public sealed class MainWindowViewModel : ViewModelBase, ICollectionOrganization
     public ReactiveCommand<Unit, Unit> ImportCollectionsFromFileCmd { get; }
 
     [Reactive]
-    public bool IsSavedLabelVisible { get; set; }
+    public bool IsTopRightLabelVisible { get; set; }
+
+    [Reactive]
+    public string TopRightLabelText { get; set; }
 
     public ReactiveCommand<Unit, Unit> SaveAllCmd { get; }
 
@@ -208,7 +211,8 @@ public sealed class MainWindowViewModel : ViewModelBase, ICollectionOrganization
         CollectionsGroupViewDataCtx = new(this, SwitchVisiblePage);
         ImportCollectionsFromFileCmd = ReactiveCommand.CreateFromTask(ImportCollectionsAsync);
         AddNewCollectionCmd = ReactiveCommand.Create(AddNewCollection);
-        IsSavedLabelVisible = false;
+        IsTopRightLabelVisible = false;
+        TopRightLabelText = string.Empty;
         SaveAllCmd = ReactiveCommand.CreateFromTask(SaveAllAsync);
         #endregion
 
@@ -361,10 +365,14 @@ public sealed class MainWindowViewModel : ViewModelBase, ICollectionOrganization
 
     private async Task SaveAllAsync()
     {
-        SaveUserData();
-        IsSavedLabelVisible = true;
-        await Task.Delay(3000);
-        IsSavedLabelVisible = false;
+        if (CanSaveUserData())
+        {
+            SaveUserData();
+            TopRightLabelText = Localizer.Instance.TopMenu.Saved;
+            IsTopRightLabelVisible = true;
+            await Task.Delay(3000);
+            IsTopRightLabelVisible = false;
+        }        
     }
 
     #endregion
@@ -471,6 +479,11 @@ public sealed class MainWindowViewModel : ViewModelBase, ICollectionOrganization
     private void LoadUserCollections() =>
         this.loadUserCollectionsTask = Task.Run(async () =>
         {
+            Dispatcher.UIThread.Invoke(() =>
+            {
+                TopRightLabelText = Localizer.Instance.TopMenu.Loading;
+                IsTopRightLabelVisible = true;
+            });
             await foreach (var col in UserDataManager.LoadUserCollectionsAsync())
             {
                 // needs to be Invoke() instead of Post().
@@ -478,12 +491,17 @@ public sealed class MainWindowViewModel : ViewModelBase, ICollectionOrganization
                 // Post() -> enqueue for future (but soon) execution
                 Dispatcher.UIThread.Invoke(() => AddCollection(col));
             }
+            await Task.Delay(800);
+            Dispatcher.UIThread.Invoke(() => IsTopRightLabelVisible = false);
         });
+
+    private bool CanSaveUserData() =>
+        this.loadUserCollectionsTask != null && this.loadUserCollectionsTask.IsCompleted;
 
     public void SaveUserData()
     {
         // This means that the user data is still being loaded.
-        if (this.loadUserCollectionsTask == null || this.loadUserCollectionsTask.IsCompleted == false)
+        if (CanSaveUserData() == false)
         {
             // IMPORTANT!
             // This protects against deleting all collections
