e414732006387b3a9309c25e35a50b397b84db29
Alexandre H.T.R. Bonfitto <alexandrehtrb@outlook.com>
2025-10-31 14:38:56 -0300

    refactor: use switch-case in cURL req importer

--- src/Pororoca.Domain/Features/ImportRequest/CurlRequestImporter.cs
+++ src/Pororoca.Domain/Features/ImportRequest/CurlRequestImporter.cs
@@ -43,173 +43,188 @@ public static partial class CurlRequestImporter
             {
                 // cURL docs: https://curl.se/docs/manpage.html
                 // if param value begins with @, then it's a file
-
-                Action act = kv.Key switch
-                {
-                    "-0" or "--http1.0" => () => httpVersion = 1.0m,
-                    "--http1.1" => () => httpVersion = 1.1m,
-                    "--http2" or "--http2-prior-knowledge" => () => httpVersion = 2.0m,
-                    "--http3" or "--http3-only" => () => httpVersion = 3.0m,
-
-                    "--basic" => () => authMode = PororocaRequestAuthMode.Basic,
-                    "--oauth2-bearer" => () =>
-                    {
-                        authMode = PororocaRequestAuthMode.Bearer;
-                        bearerToken = kv.Value;
-                    },
-                    "--ntlm" => () => authMode = PororocaRequestAuthMode.Windows,
-                    "-u" or "--user" => () =>
-                    {
-                        var (usr, pwd) = SplitKeyValueSeparatedByChar(kv.Value, ':');
-                        authUser = usr;
-                        authPassword = pwd;
-                    },
-
-                    "--cert-type" => () =>
-                    {
-                        authCertType = kv.Value switch
-                        {
-                            "PEM" => PororocaRequestAuthClientCertificateType.Pem,
-                            "P12" => PororocaRequestAuthClientCertificateType.Pkcs12,
-                            _ => PororocaRequestAuthClientCertificateType.Pem,
-                        };
-                    },
-                    "-E" or "--cert" => () =>
-                    {
-                        authMode = PororocaRequestAuthMode.ClientCertificate;
-                        authCertFilePath = kv.Value;
-                    },
-                    "--key" => () => authCertPrivateKeyFilePath = kv.Value,
-                    "--pass" => () => authCertPrivateKeyPassword = kv.Value,
-
-                    "-X" or "--request" => () => httpMethodFromRequest = kv.Value,
-                    "-G" or "--get" => () => httpMethodFromRequest = "GET",
-
-                    "--url" => () => url = kv.Value,
-                    "--url-query" => () =>
-                    {
-                        urlQueryKvs ??= new();
-                        urlQueryKvs.Add(kv.Value);
-                    },
-
-                    "-H" or "--header" => () =>
-                    {
-                        var (headerName, headerValue) = SplitKeyValueSeparatedByChar(kv.Value, ':');
-
-                        if (headerName == "Content-Type")
-                        {
-                            bodyContentType = headerValue;
-                        }
-                        else
-                        {
-                            headers ??= new();
-                            headers.Add(new(true, headerName, headerValue));
-                        }
-                    },
-                    "-e" or "--referer" => () =>
-                    {
-                        headers ??= new();
-                        headers.Add(new(true, "Referer", kv.Value));
-                    },
-                    "-A" or "--user-agent" => () =>
-                    {
-                        headers ??= new();
-                        headers.Add(new(true, "User-Agent", kv.Value));
-                    },
-
-                    "-d" or
-                    "--data" or
-                    "--data-ascii" or
-                    "--data-raw" or
-                    "--data-urlencode" or
-                    "--data-binary" => () =>
-                    {
-                        httpMethodFromData = "POST";                        
-                        if (hasMultipleDataParams)
-                        {
-                            bodyMode = PororocaHttpRequestBodyMode.UrlEncoded;
-                            var (key, value) = SplitKeyValueSeparatedByChar(kv.Value, '=');
-                            urlEncodedParams ??= new();
-                            urlEncodedParams.Add(new(true, key, value));
-                        }
-                        else if (kv.Value.StartsWith('@'))
-                        {
-                            bodyMode = PororocaHttpRequestBodyMode.File;
-                            bodyFilePath = kv.Value.TrimStart('@');
-                            MimeTypesDetector.TryFindMimeTypeForFile(bodyFilePath, out bodyContentType);
-                        }
-                        else
-                        {
-                            bodyMode = PororocaHttpRequestBodyMode.Raw;
-                            bodyRaw = kv.Value;
-                            bodyContentType ??= MimeTypesDetector.DefaultMimeTypeForText;
-                        }
-                    },
-                    "-F" or "--form" => () =>
-                    {
-                        httpMethodFromData = "POST";
-                        bodyMode = PororocaHttpRequestBodyMode.FormData;
-                        string? formParamContentType = null;
-                        string formParamKey, formParamValue;
-                        if (kv.Value.Contains(';'))
-                        {
-                            var (part1, part2) = SplitKeyValueSeparatedByChar(kv.Value, ';');
-                            if (part2.StartsWith("type"))
-                            {
-                                formParamContentType = SplitKeyValueSeparatedByChar(part2, '=').Item2;
-                            }
-                            (formParamKey, formParamValue) = SplitKeyValueSeparatedByChar(part1, '=');
-                        }
-                        else
-                        {
-                            (formParamKey, formParamValue) = SplitKeyValueSeparatedByChar(kv.Value, '=');
-                        }
-
-                        formDataParams ??= new();
-                        if (formParamValue.StartsWith('@'))
-                        {
-                            if (formParamContentType == null)
-                            {
-                                MimeTypesDetector.TryFindMimeTypeForFile(formParamValue, out formParamContentType);
-                            }
-                            formDataParams.Add(MakeFileParam(true, formParamKey, formParamValue.TrimStart('@'), formParamContentType ?? MimeTypesDetector.DefaultMimeTypeForText));
-                        }
-                        else
-                        {
-                            formDataParams.Add(MakeTextParam(true, formParamKey, formParamValue, formParamContentType ?? MimeTypesDetector.DefaultMimeTypeForText));
-                        }
-                    },
-                    "--json" => () =>
-                    {
-                        httpMethodFromData = "POST";
-                        headers ??= new();
-                        headers.Add(new(true, "Accept", MimeTypesDetector.DefaultMimeTypeForJson));
-                        bodyContentType = MimeTypesDetector.DefaultMimeTypeForJson;
-                        if (kv.Value.StartsWith('@'))
-                        {
-                            bodyMode = PororocaHttpRequestBodyMode.File;
-                            bodyFilePath = kv.Value.TrimStart('@');
-                        }
-                        else
-                        {
-                            bodyMode = PororocaHttpRequestBodyMode.Raw;
-                            bodyRaw = kv.Value;
-                        }
-                    },
-                    "-T" or "--upload-file" => () =>
-                    {
-                        httpMethodFromData = "PUT";
-                        bodyMode = PororocaHttpRequestBodyMode.File;
-                        bodyFilePath = kv.Value;
-                        MimeTypesDetector.TryFindMimeTypeForFile(bodyFilePath, out bodyContentType);
-                    },
-
-                    _ => () => { }
-                };
-
                 try
                 {
-                    act();
+                    switch (kv.Key)
+                    {
+                        case "-0":
+                        case "--http1.0":
+                            httpVersion = 1.0m;
+                            break;
+                        case "--http1.1":
+                            httpVersion = 1.1m;
+                            break;
+                        case "--http2":
+                        case "--http2-prior-knowledge":
+                            httpVersion = 2.0m;
+                            break;
+                        case "--http3":
+                        case "--http3-only":
+                            httpVersion = 3.0m;
+                            break;
+
+                        case "--basic":
+                            authMode = PororocaRequestAuthMode.Basic;
+                            break;
+                        case "--oauth2-bearer":
+                            ;
+                            authMode = PororocaRequestAuthMode.Bearer;
+                            bearerToken = kv.Value;
+                            break;
+                        case "--ntlm":
+                            authMode = PororocaRequestAuthMode.Windows;
+                            break;
+                        case "-u":
+                        case "--user":
+                            var (usr, pwd) = SplitKeyValueSeparatedByChar(kv.Value, ':');
+                            authUser = usr;
+                            authPassword = pwd;
+                            break;
+
+                        case "--cert-type":
+                            authCertType = kv.Value == "P12" ?
+                                PororocaRequestAuthClientCertificateType.Pkcs12 :
+                                PororocaRequestAuthClientCertificateType.Pem;
+                            break;
+                        case "-E":
+                        case "--cert":
+                            authMode = PororocaRequestAuthMode.ClientCertificate;
+                            authCertFilePath = kv.Value;
+                            break;
+                        case "--key":
+                            authCertPrivateKeyFilePath = kv.Value;
+                            break;
+                        case "--pass":
+                            authCertPrivateKeyPassword = kv.Value;
+                            break;
+
+                        case "-X":
+                        case "--request":
+                            httpMethodFromRequest = kv.Value;
+                            break;
+                        case "-G":
+                        case "--get":
+                            httpMethodFromRequest = "GET";
+                            break;
+
+                        case "--url":
+                            url = kv.Value;
+                            break;
+                        case "--url-query":
+                            urlQueryKvs ??= new();
+                            urlQueryKvs.Add(kv.Value);
+                            break;
+                        case "-H":
+                        case "--header":
+                            var (headerName, headerValue) = SplitKeyValueSeparatedByChar(kv.Value, ':');
+                            if (headerName == "Content-Type")
+                            {
+                                bodyContentType = headerValue;
+                            }
+                            else
+                            {
+                                headers ??= new();
+                                headers.Add(new(true, headerName, headerValue));
+                            }
+                            break;
+                        case "-e":
+                        case "--referer":
+                            headers ??= new();
+                            headers.Add(new(true, "Referer", kv.Value));
+                            break;
+                        case "-A":
+                        case "--user-agent":
+                            headers ??= new();
+                            headers.Add(new(true, "User-Agent", kv.Value));
+                            break;
+
+                        case "-d":
+                        case "--data":
+                        case "--data-ascii":
+                        case "--data-raw":
+                        case "--data-urlencode":
+                        case "--data-binary":
+                            httpMethodFromData = "POST";
+                            if (hasMultipleDataParams)
+                            {
+                                bodyMode = PororocaHttpRequestBodyMode.UrlEncoded;
+                                var (key, value) = SplitKeyValueSeparatedByChar(kv.Value, '=');
+                                urlEncodedParams ??= new();
+                                urlEncodedParams.Add(new(true, key, value));
+                            }
+                            else if (kv.Value.StartsWith('@'))
+                            {
+                                bodyMode = PororocaHttpRequestBodyMode.File;
+                                bodyFilePath = kv.Value.TrimStart('@');
+                                MimeTypesDetector.TryFindMimeTypeForFile(bodyFilePath, out bodyContentType);
+                            }
+                            else
+                            {
+                                bodyMode = PororocaHttpRequestBodyMode.Raw;
+                                bodyRaw = kv.Value;
+                                bodyContentType ??= MimeTypesDetector.DefaultMimeTypeForText;
+                            }
+                            break;
+                        case "-F":
+                        case "--form":
+                            httpMethodFromData = "POST";
+                            bodyMode = PororocaHttpRequestBodyMode.FormData;
+                            string? formParamContentType = null;
+                            string formParamKey, formParamValue;
+                            if (kv.Value.Contains(';'))
+                            {
+                                var (part1, part2) = SplitKeyValueSeparatedByChar(kv.Value, ';');
+                                if (part2.StartsWith("type"))
+                                {
+                                    formParamContentType = SplitKeyValueSeparatedByChar(part2, '=').Item2;
+                                }
+                                (formParamKey, formParamValue) = SplitKeyValueSeparatedByChar(part1, '=');
+                            }
+                            else
+                            {
+                                (formParamKey, formParamValue) = SplitKeyValueSeparatedByChar(kv.Value, '=');
+                            }
+
+                            formDataParams ??= new();
+                            if (formParamValue.StartsWith('@'))
+                            {
+                                if (formParamContentType == null)
+                                {
+                                    MimeTypesDetector.TryFindMimeTypeForFile(formParamValue, out formParamContentType);
+                                }
+                                formDataParams.Add(MakeFileParam(true, formParamKey, formParamValue.TrimStart('@'), formParamContentType ?? MimeTypesDetector.DefaultMimeTypeForText));
+                            }
+                            else
+                            {
+                                formDataParams.Add(MakeTextParam(true, formParamKey, formParamValue, formParamContentType ?? MimeTypesDetector.DefaultMimeTypeForText));
+                            }
+                            break;
+                        case "--json":
+                            httpMethodFromData = "POST";
+                            headers ??= new();
+                            headers.Add(new(true, "Accept", MimeTypesDetector.DefaultMimeTypeForJson));
+                            bodyContentType = MimeTypesDetector.DefaultMimeTypeForJson;
+                            if (kv.Value.StartsWith('@'))
+                            {
+                                bodyMode = PororocaHttpRequestBodyMode.File;
+                                bodyFilePath = kv.Value.TrimStart('@');
+                            }
+                            else
+                            {
+                                bodyMode = PororocaHttpRequestBodyMode.Raw;
+                                bodyRaw = kv.Value;
+                            }
+                            break;
+                        case "-T":
+                        case "--upload-file":
+                            httpMethodFromData = "PUT";
+                            bodyMode = PororocaHttpRequestBodyMode.File;
+                            bodyFilePath = kv.Value;
+                            MimeTypesDetector.TryFindMimeTypeForFile(bodyFilePath, out bodyContentType);
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 catch
                 {
